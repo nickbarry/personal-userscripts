cb3ed1af682f3f3784aa311b4226b7913be83787
Nick Barry <niko.barry@gmail.com>
2015-10-11 19:10:08 -0700

    Add update/download urls

--- Todoist search field editor.user.js
+++ Todoist search field editor.user.js
@@ -1,12 +1,14 @@
 // ==UserScript== 
-// @name Todoist search field editor 
-// @namespace http://nicholasbarry.com/ 
-// @version 0.1 
-// @description Allows a user to edit the current search query in Todoist 
-// @author Nicholas Barry 
-// @match http*://*todoist.com/app* 
-// @require http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js 
-// @grant none 
+// @name 		 Todoist search field editor 
+// @namespace 	 http://nicholasbarry.com/ 
+// @version 	 0.1 
+// @updateURL    https://github.com/nickbarry/personal-userscripts/raw/master/Todoist%20search%20field%20editor.user.js
+// @downloadURL  https://github.com/nickbarry/personal-userscripts/raw/master/Todoist%20search%20field%20editor.user.js
+// @description  Allows a user to edit the current search query in Todoist 
+// @author 	 	 Nicholas Barry 
+// @match 		 http*://*todoist.com/app* 
+// @require 	 http://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js 
+// @grant 		 none 
 // ==/UserScript== 
 
 this.$ = this.jQuery = jQuery.noConflict(true); 
