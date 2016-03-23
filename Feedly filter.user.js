a19be96a0fb9f8c3d86ce4f18acef8085968010b
Nick Barry <niko.barry@gmail.com>
2016-03-22 18:30:30 -0700

    Update version number

--- Feedly filter.user.js
+++ Feedly filter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Feedly filter
-// @version      1.2.2
+// @version      1.2.3
 // @update	 https://github.com/nickbarry/personal-userscripts/raw/master/Feedly%20filter.user.js
 // @description  Filter out feedly articles according to certain keywords
 // @author       Nico Barry
