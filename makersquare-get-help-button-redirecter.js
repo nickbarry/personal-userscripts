fea0c907ed70f778a20c5f17139af0808ad6d646
Nick Barry <niko.barry@gmail.com>
2016-06-09 08:06:18 -0700

    new terms

---
+++ makersquare-get-help-button-redirecter.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Bookstrap help link redirecter
+// @version      0.1
+// @description  Redirect the help link
+// @author       Nico Greenarry
+// @include        http://bookstrap.makersquare.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  console.log('We are running!');
+
+})();
