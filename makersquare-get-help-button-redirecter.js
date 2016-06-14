6c7be24633b32d1f1107e9f510f587551ed774ac
Nick Barry <niko.barry@gmail.com>
2016-06-13 18:27:30 -0700

    Add userscript to redirect the Bookstrap help button
    The help button in Bookstrap is deprecated. This userscript makes the button redirect to the actual help page.

--- makersquare-get-help-button-redirecter.js
+++ makersquare-get-help-button-redirecter.js
@@ -10,6 +10,12 @@
 (function() {
   'use strict';
 
-  console.log('We are running!');
+  function replaceGetHelp(){
+    var $getHelp = $('.get-help-tab');
 
+    var $newHelp = $('<a href="http://helpdesk.makerpass.com/groups/mks-40/tickets" target="_blank">Get Help</a>').addClass('get-help-tab help-tab ng-scope');
+    $getHelp.hide().after($newHelp);
+  }
+
+  setTimeout(replaceGetHelp,5000);
 })();
