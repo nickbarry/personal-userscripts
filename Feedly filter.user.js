88bf05b3d5ad097d3da787a5b95b0e99b903dc4b
Nick Barry <niko.barry@gmail.com>
2016-06-14 13:09:40 -0700

    Refactor --> pseudo-classical style
    Move the functions and variables into a pseudoclassical constructor, with private variables and functions, and properties and methods.
    
    Also add a couple of terms.

--- Feedly filter.user.js
+++ Feedly filter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Feedly filter
-// @version      1.4
+// @version      1.5
 // @update	 https://github.com/nickbarry/personal-userscripts/raw/master/Feedly%20filter.user.js
 // @description  Filter out feedly articles according to certain keywords
 // @author       Nico Barry
@@ -13,7 +13,16 @@
 // Latest version here:
 // https://github.com/nickbarry/personal-userscripts/blob/master/Feedly%20filter.user.js
 
-var termsToExclude = [
+var FilterMaker = (function(){
+  var FilterMaker = function(){
+    this.uniqueArticleTitles = [];
+    this.latestArticle = 0;
+    this.currentUrl = '';
+    this.counter = 0; // A counter I can use for various testing scenarios
+  }
+
+  // PRIVATE VARIABLES
+  var termsToExclude = [
     /* Temp items */ /\bprince/,
 
     /* Pop culture */ /kardashian/,/kanye/,/downton/,/walking dead/,/whiskey tango foxtrot/,/\boprah/,/kate hudson/,
@@ -21,10 +30,10 @@ var termsToExclude = [
     /Jay Z/,/divergent/,/lil'? kim/,/netflix/,/\brapper/,/ke[s$]ha/,/instagram/,/tidal/,/mtv/,/coachella/,/espn/,
     /cable box/,/roku/,/samantha bee/,/full frontal/,/kylie jenner/,/bruce jenner/,/doctor who/,/beyonc[eé]/,/beyhive/,
     /hunger games/,/tony award/,/tony'?s/,/hollywood/,/powerball/,/captain america/,/bieber/,/george r\. ?r\. martin/,
-    /half life/,/thor/,
+    /half life/,/thor/,/season \d/,
 
     /* Apple stuff */ /\bmacs?\b/,/ipad/,/apple watch/,/smartwatch/,/\bos ?x\b/,/ios game/,/apple game/,/ios app/,
-    /\bios/,/watchband/,/iphone se/,/macbook/,/lightning cable/,/apple music/,/icloud/,/\bmacs\b/,/imessage/,
+    /\bios/,/watchband/,/macbook/,/lightning cable/,/apple music/,/icloud/,/\bmacs\b/,/imessage/,
     /\bmacos\b/,/\bapple/,/iphone/,/opera/,/safari/,
 
     /* Technology */ /nvidia/,/\bhdr\b/,/\bacer\b/,/ps4/,/\bnes\b/,
@@ -57,111 +66,111 @@ var termsToExclude = [
 
     /* Misc */ /mcdonald's/];
 
-var App = {
-  detectArticleTimer: window.setInterval(detectArticles, 300),
-  uniqueArticleTitles: [],
-  latestArticle: 0,
-  currentUrl: '',
-  counter: 0 // A counter I can use for various testing scenarios
-};
-
-// HOISTED FUNCTIONS
-// Caching function
-function memoize(f) {
-  var cache = {};
-
-  var memoizedFn = function() {
-    var arg_str = JSON.stringify(arguments);
-    cache[arg_str] = cache[arg_str] || f.apply(f, arguments);
-    return cache[arg_str];
-  };
-
-  memoizedFn.clear = function(){ // allow user to clear the cache of the memoized function
-    cache = {};
-  };
-
-  return memoizedFn;
-}
-
-function pageChangeReset(App, memoizedFn){
-  console.log('Resetting App');
-  App.currentUrl = window.location.href;
-  App.latestArticle = 0;
-  App.uniqueArticleTitles.length = 0;
-  if(memoizedFn && typeof memoizedFn === 'function') { // If there is a memoized function passed in, clear its cache
-    memoizedFn.clear();
-  }
-}
-
-// Test whether a particular string contains one of the terms I'd like to exclude
-var hasExcludedTerm = function(titleString){
-  var result = {hasTerm: false},
-    titleLower = titleString.toLowerCase(); // assume title does not have an excluded term
-  for(var i = 0; i < termsToExclude.length; i++){
-    if(titleLower.search(termsToExclude[i]) !== -1){ // title has [i] excluded term in it
-      result.hasTerm = true;
-      result.term = termsToExclude[i];
-      break;
+  // Once articles are detected, set a regular interval to review and hide newly-loaded articles that match exclusion terms
+  FilterMaker.prototype.detectArticles = function(){
+    var articles = articlesExist();
+    if(articles){
+      window.setInterval(this.reviewArticles.bind(this), 5000);
+      window.clearInterval(this.detectArticleTimer);
     }
   }
-  return result;
-};
 
-var isTitleUnique = function isTitleUnique(title,arr){
-  return !~arr.indexOf(title); // true if the title is NOT found in the array
-};
-
-// Convert array-like object to array
-function toArray(arrayLikeObj){
-	return [].slice.call(arrayLikeObj);
-}
-
-// Checks if the articles have loaded yet in Feedly; if so, returns the article titles array
-function articlesExist(){
-  var articles = toArray(document.getElementsByClassName('u0Entry'));
-  if(articles.length){
-    return articles;
-  }else{
-    return false;
+  FilterMaker.prototype.pageChangeReset = function(memoizedFn){
+    this.currentUrl = window.location.href;
+    this.latestArticle = 0;
+    this.uniqueArticleTitles.length = 0;
+    if(memoizedFn && typeof memoizedFn === 'function') { // If there is a memoized function passed in, clear its cache
+      memoizedFn.clear();
+    }
   }
-}
 
-// Once articles are detected, set a regular interval to review and hide newly-loaded articles that match exclusion terms
-function detectArticles(){
-  var articles = articlesExist();
-  if(articles){
-    window.setInterval(reviewArticles, 5000);
-    //console.log('articles length: ',articles.length);
-    window.clearInterval(App.detectArticleTimer);
-  }
-}
+  // Marks articles as hidden and read if they have an excluded term in them
+  FilterMaker.prototype.reviewArticles = function(){
+    if(this.currentUrl !== window.location.href) {this.pageChangeReset();}
 
-// Marks articles as hidden and read if they have an excluded term in them
-function reviewArticles(){
-  if(App.currentUrl !== window.location.href) {pageChangeReset(App);}
+    var articles = articlesExist();
+    var titles = articles.map(function(article){
+      return article.dataset.title;
+    });
 
-  var articles = articlesExist();
-  var titles = articles.map(function(article){
-    return article.dataset.title;
-  });
+    for(var i = this.latestArticle + 1; i < titles.length; i++){
+      var title = titles[i];
 
-  for(var i = App.latestArticle + 1; i < titles.length; i++){
-    var title = titles[i];
-    if(isTitleUnique(title,App.uniqueArticleTitles)){
-      App.uniqueArticleTitles.push(title); // If it is unique, add it to the array
+      if(isTitleUnique(title,this.uniqueArticleTitles)){
+        this.uniqueArticleTitles.push(title); // If it is unique, add it to the array
 
-      var excludedTermCheck = hasExcludedTerm(title); // Check if title has a term I'd like to exclude
-      if(excludedTermCheck.hasTerm){
-        removeTitle(articles[i],title,excludedTermCheck);
+        var excludedTermCheck = hasExcludedTerm(title); // Check if title has a term I'd like to exclude
+        if(excludedTermCheck.hasTerm){
+          removeTitle(articles[i],title,excludedTermCheck);
+        }
+      }else{
+        removeTitle(articles[i],title,{term: 'Duplicate'});
       }
+    }
+    this.latestArticle = i; // Update my latest article tracker
+  }
+
+  return FilterMaker;
+
+  // PRIVATE, HOISTED FUNCTIONS
+  // Caching function
+  function memoize(f) {
+    var cache = {};
+
+    var memoizedFn = function() {
+      var arg_str = JSON.stringify(arguments);
+      cache[arg_str] = cache[arg_str] || f.apply(f, arguments);
+      return cache[arg_str];
+    };
+
+    memoizedFn.clear = function(){ // allow user to clear the cache of the memoized function
+      cache = {};
+    };
+
+    return memoizedFn;
+  }
+
+  // Test whether a particular string contains one of the terms I'd like to exclude
+  function hasExcludedTerm(titleString){
+    var result = {hasTerm: false},
+      titleLower = titleString.toLowerCase(); // assume title does not have an excluded term
+    for(var i = 0; i < termsToExclude.length; i++){
+      if(titleLower.search(termsToExclude[i]) !== -1){ // title has [i] excluded term in it
+        result.hasTerm = true;
+        result.term = termsToExclude[i];
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  function isTitleUnique(title,arr){
+    return !~arr.indexOf(title); // true if the title is NOT found in the array
+  }
+
+  // Convert array-like object to array
+  function toArray(arrayLikeObj){
+    return [].slice.call(arrayLikeObj);
+  }
+
+  // Checks if the articles have loaded yet in Feedly; if so, returns the article titles array
+  function articlesExist(){
+    var articles = toArray(document.getElementsByClassName('u0Entry'));
+    if(articles.length){
+      return articles;
     }else{
-      removeTitle(articles[i],title,{term: 'Duplicate'});
+      return false;
     }
   }
-  App.latestArticle = i; // Update my latest article tracker
-}
 
-function removeTitle(article,title,excludedTermCheck){
-  console.log(excludedTermCheck.term + ": " + title);
-  article.children[0].children[3].click();
-}
+  function removeTitle(article,title,excludedTermCheck){
+    console.log(excludedTermCheck.term + ": " + title);
+    article.children[0].children[3].click();
+  }
+})();
+
+var Filter = new FilterMaker();
+
+Filter.detectArticleTimer = window.setInterval(Filter.detectArticles.bind(Filter), 300);
+
