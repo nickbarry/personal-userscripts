c3c99849d70e5e369771f155cce45b2baf4a69fc
Nick Barry <niko.barry@gmail.com>
2016-08-08 09:56:16 -0700

    Add/update terms

--- Feedly filter.user.js
+++ Feedly filter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Feedly filter - BETA
-// @version      3.2.17
+// @version      3.2.18
 // @update	 https://github.com/nickbarry/personal-userscripts/raw/master/Feedly%20filter.user.js
 // @description  Filter out feedly articles according to certain keywords
 // @author       Nico Barry
@@ -54,7 +54,7 @@ var FilterMaker = (function(){
     /half[ -]?life/,/\bthor\b/,/season \d/,/orange.*new black/,/the bachelor/,/yelchin/,/taylor swift/,/suicide squad/,
     /star trek/,/trekkie/,/ghost ?buster/,/power ranger/,/warcraft/,/trump time capsule/,/big brother/,/bet award/,
     /season premiere/,/season \d/,/broadway/,/america's got talent/,/zelda/,/binge[ -]?watch/,/pokemon/,/mr. robot/,
-    /appelbaum/,/\bolympi/,/\bpoké/,/pok[ée]mon/,
+    /appelbaum/,/\bolympi/,/\bpoké/,/pok[ée]mon/,/hulu/,
 
     /* Politics */ /hastert/,
 
